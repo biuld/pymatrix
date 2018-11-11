b32ac80e9c3c66413dbce5c6d53bc5d5edc7c6a4
biuld <xboltk@gmail.com>
2018-11-11 16:56:49 +0800

    improved performance

--- async_pymatrix.py
+++ async_pymatrix.py
@@ -3,47 +3,31 @@
 import curses
 import asyncio
 import random
+import sys
 
 
 class playground(object):
 
     def __init__(self, stdscr):
         self.stdscr = stdscr
-        maxLine = curses.LINES
-        maxColumn = curses.COLS
+        maxLine, maxColumn = stdscr.getmaxyx()
+
+        self.top = 0
+        self.left = 0
+        self.right = maxColumn - 1
+        self.bottom = maxLine - 1
+
+        curses.use_default_colors()
+        curses.curs_set(False)
 
         random.seed()
 
-        self.top = int(maxLine * 0.1)
-        self.bottom = int(maxLine * 0.8)
-        self.left = int(maxColumn * 0.1)
-        self.right = int(maxColumn * 0.9)
+    def display_msg(self, msg):
+        self.stdscr.addstr(0, 0, str(msg))
+        self.stdscr.refresh()
 
-        curses.init_pair(1, curses.COLOR_YELLOW, curses.COLOR_BLACK)
-        curses.init_pair(2, curses.COLOR_GREEN, curses.COLOR_BLACK)
-        curses.curs_set(False)
-
-        self.__drawboder(stdscr, self.top, self.bottom, self.left, self.right)
-
-    def __drawboder(self, stdscr, top, bottom, left, right):
-        stdscr.addstr(top, left, "+")
-        stdscr.addstr(bottom, left, "+")
-        stdscr.addstr(top, right, "+")
-        stdscr.addstr(bottom, right, "+")
-
-        for i in range(top + 1, bottom):
-            stdscr.addstr(i, left, "|")
-            stdscr.addstr(i, right, "|")
-
-        for i in range(left + 1, right):
-            stdscr.addstr(top, i, "-")
-            stdscr.addstr(bottom, i, "-")
-
-        stdscr.refresh()
-
-    async def matrix(self, y):
-        attr = curses.color_pair(2)
-        length = random.randint(1, 15)
+    async def matrix(self, target):
+        attr = curses.COLOR_GREEN
 
         def gen_bin(length):
             binary = []
@@ -54,39 +38,42 @@ class playground(object):
             while True:
                 yield from binary
 
-        message = gen_bin(length)
         position = []
 
-        await asyncio.sleep(random.randint(0, 5000) * 0.001)
-
         while True:
+
             if len(position) == 0:
-                position = [self.top + 1]
-                self.stdscr.addstr(position[0], y, next(message), attr)
+                position = [self.top]
                 length = random.randint(1, 15)
+                message = gen_bin(length)
+                self.stdscr.addstr(position[0], target, next(message), attr)
 
-            if position[0] < self.bottom - 1:
+            if position[0] < self.bottom:
                 position.insert(0, position[0] + 1)
-                self.stdscr.addstr(position[0], y, next(message), attr)
+                self.stdscr.addstr(position[0], target, next(message), attr)
 
-            if (len(position) > length or position[0] == self.bottom - 1) and len(position) > 0:
-                self.stdscr.addstr(position.pop(), y, " ")
+            if (len(position) > length or position[0] == self.bottom) and len(position) > 0:
+                self.stdscr.addstr(position.pop(), target, " ")
 
             self.stdscr.refresh()
-
             await asyncio.sleep(0.05)
 
     async def supervisor(self):
 
         tasks = []
-        for i in range(self.left + 1, self.right - 1):
+
+        th = self.right - self.left
+
+        self.display_msg("current coroutines: %d" % th)
+
+        for i in range(self.left, self.right):
             tasks.append(asyncio.ensure_future(self.matrix(i)))
 
         try:
             await asyncio.gather(*tasks)
-        except asyncio.CancelledError:
-            for task in tasks:
-                task.cancel()
+        except:
+            for i in tasks:
+                i.cancel()
 
 
 def main(stdscr):
